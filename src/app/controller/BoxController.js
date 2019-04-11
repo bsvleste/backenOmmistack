58bd2a779216381e89140f517a9bc1655b971314
Bruno de Souza Valeiro <bvaleiro85@hotmail.com>
2019-04-11 16:49:18 -0300

    testea

--- src/app/controller/BoxController.js
+++ src/app/controller/BoxController.js
@@ -10,11 +10,12 @@ router.post('/create',async(req,res)=>{
     return res.json(box);
 });
 
-router.get('/boxs',async(req,res)=>{
+/* router.get('/boxs',async(req,res)=>{
     const lista = await Box.find().populate({
         path:'file',
         options:{sort:{createdAt:-1}}
     });
     return res.json(lista);
-});
+}); */
+
 module.exports = app=>app.use('/box',router);
